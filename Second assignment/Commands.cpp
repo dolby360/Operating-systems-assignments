b9272402a54d712f8bde364bbb7c0acac872057d
dolby360 <mrdolbyking@gmail.com>
2018-03-16 13:19:15 +0200

    Support environment variable

--- Second assignment/Commands.cpp
+++ Second assignment/Commands.cpp
@@ -23,27 +23,62 @@ std::vector<std::string> Commands::pushTokens(std::string line){
 
 
 std::vector<std::string> Commands::eval(std::vector<std::string> vec){
+    char *env;
     std::vector<std::string> newVec(vec);
 
     //For every element in the vector
-    for(ui i = 0; i < vec.size();i++){ 
+    for(ui i = 0; i < vec.size();i++){
         //If first charecter equal to ~ then replace it with getenv("HOME");
         if(vec[i].compare(0,1,"~")==0){
             vec[i] = std::string(getenv("HOME")).append(vec[i].erase(0,1));
         }
-        //For every word in the element
+        //For every letter in the element
         for(ui j = 0; j < vec[i].length()-1 ; j++){ 
             //Replace $? with exit command. 
             if(vec[i].compare(j,1,"$") == 0 && vec[i].compare(j+1,1,"?") == 0){
                 vec[i].erase(j+1,1);
                 vec[i].replace(j,1,std::to_string(exitCode));
             }
+            //If we see enviromment variable
+            else if(vec[i].compare(j,1,"$") == 0 && j < vec[i].size() && vec[i].compare(j+1,1,"$") != 0){
+                if(
+                !(vec[i][j+1] >= 'a' && vec[i][j+1] <= 'z')&&
+                !(vec[i][j+1] >= 'A' && vec[i][j+1] <= 'Z')&&
+                !(vec[i][j+1] == '_')
+                ){continue;}
+
+                std::string tempStr;
+                ui t;
+                for(t = j+1; t < vec[i].size();t++){
+                    tempStr += vec[i][t];
+                    //Next line for debuging.
+                    //std::cout << " - " << tempStr << " - " << tempStr.back() <<"\n";
+                    if(
+                        !(tempStr.back() >= 'a' && tempStr.back() <= 'z')&&
+                        !(tempStr.back() >= 'A' && tempStr.back() <= 'Z')&&
+                        !(tempStr.back() == '_')&&
+                        !(t != 1 && tempStr.back() >= '0' && tempStr.back() <= '9')
+                        )
+                    {
+                        tempStr.erase(tempStr.size()-1,1);
+                        break;
+                    }
+                }
+                env = getenv(tempStr.c_str());
+                tempStr.clear();
+                if(env == NULL){
+                    vec[i].erase(j,t-j);
+                }else{
+                    vec[i].erase(j,t-j);
+                    vec[i].insert(j,env);
+                }
+                
+            }
         }
         //Use this line for debugging
         //It can show you how it went.
-        std::cout << vec[i] << std::endl;
+        std::cout << "-- " << vec[i] << std::endl;
     }
-    
     return newVec;
 }
 
@@ -89,8 +124,8 @@ void Commands::replaceHomePath(){
     fullPath = get_current_dir_name();
     home=getenv("HOME");
 
-    int i;    
-    for( i = 0 ; i < (int)strlen(home) ; i++ ){
+    ui i;    
+    for( i = 0 ; i < strlen(home) ; i++ ){
         if(home[i]!=fullPath[i]){
             i--;
             break;
@@ -102,8 +137,8 @@ void Commands::replaceHomePath(){
 
     memset(this->fullPath,'\0',strlen(this->fullPath));
 
-    int k = 1;
-    for(; i < (int)strlen(temp); i++,k++){
+    ui k = 1;
+    for(; i < strlen(temp); i++,k++){
         this->fullPath[k] = temp[i];
     }
     this->fullPath[0] = '~';

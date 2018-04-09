8b37bcbcbe422b88ea8c56b4507c9c42ca4c6c95
dolev <dolby360>
2018-04-09 14:30:14 +0300

    repair

--- Second assignment/compileAndRun.sh
+++ Second assignment/compileAndRun.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-if [[ -f shell  ]] 
-then
-    rm shell
-fi
+rm shell
 g++ -o shell *.cpp -std=c++11 -Wall -Wno-vla -pedantic -march=core2 -Os -pipe -fstack-protector-all -g3 -Wl,-O,1,-z,combreloc
 if [[ -f shell  ]] 
 then
